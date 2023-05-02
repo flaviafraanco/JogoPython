50a74f014ecc435b89edc4f6ceaed86c77597b9d
Flavia Franco <104872642+flaviafraanco@users.noreply.github.com>
2023-05-02 20:25:25 -0300

    inicio do jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,12 @@
-print("Bem vindo ao jogo de adivinhação")
+print("******************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("******************************")
+
+numero_secreto = 43
+
+chute= input("Digite o seu numero: ")
+
+print("Vode digitou", chute)
+
+se numero_secreto == chute
+    print("Voce acertou!")
